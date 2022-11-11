882e90e74a09e7818fe30139d56149e82c52cffa
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-11-11 11:26:40 +0100

    posts saving and loading

--- src/main/java/cz/mk/resources/PostsResource.java
+++ src/main/java/cz/mk/resources/PostsResource.java
@@ -1,18 +1,41 @@
 package cz.mk.resources;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import cz.mk.gson.deserializers.LocalDateTimeDeserializer;
+import cz.mk.gson.serializers.LocalDateTimeSerializer;
 import cz.mk.interfaces.resources.IPostsResource;
 import cz.mk.models.Post;
 
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PostsResource implements IPostsResource {
-    @Override
-    public void save(List<Post> postList) {
-
-    }
+    private Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer())
+            .create();
 
     @Override
     public List<Post> load() {
-        return null;
+        List<Post> list = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("simple-cms-jsf-data.json"));
+            String json = br.readLine();
+            list = new ArrayList<>(List.of(gson.fromJson(json, Post[].class)));
+            br.close();
+        } catch (Exception ignored) {}
+        return list;
+    }
+
+    @Override
+    public void save(List<Post> posts) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("simple-cms-jsf-data.json", false));
+            bw.write(gson.toJson(posts));
+            bw.close();
+        } catch (IOException ignored) {}
     }
 }
