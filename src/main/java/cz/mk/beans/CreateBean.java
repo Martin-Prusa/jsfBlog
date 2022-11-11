31a2463e7f6bc0469c43f437d7359b342ccaf68e
karel <karel.fila@educanet.cz>
2022-11-11 11:46:19 +0100

    vypsaní fromuláře do konzole

--- src/main/java/cz/mk/beans/CreateBean.java
+++ src/main/java/cz/mk/beans/CreateBean.java
@@ -1,4 +1,40 @@
 package cz.mk.beans;
 
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
 public class CreateBean {
+    private String title;
+    private String shortDes;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortDes() {
+        return shortDes;
+    }
+
+    public void setShortDes(String shortDes) {
+        this.shortDes = shortDes;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void submitForm() {
+        System.out.println(title + shortDes + content);
+    }
 }
