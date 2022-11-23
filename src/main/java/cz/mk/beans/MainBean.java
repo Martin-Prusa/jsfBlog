19efddf1bd167b7431630bdbc7dbb03a2bb565a1
Martin Průša <martinprusa@Martin-iMac-24.localdomain>
2022-11-23 15:50:24 +0100

    DetailBean - getPost()

--- src/main/java/cz/mk/beans/MainBean.java
+++ src/main/java/cz/mk/beans/MainBean.java
@@ -1,4 +1,24 @@
 package cz.mk.beans;
 
+import cz.mk.interfaces.services.IPostsService;
+import cz.mk.models.Post;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.util.List;
+
+@Named
+@ApplicationScoped
 public class MainBean {
+    @Inject
+    private IPostsService postsService;
+
+    public void deletePost(String id) {
+        this.postsService.deletePost(id);
+    }
+
+    public List<Post> getPosts() {
+        return postsService.getPosts();
+    }
 }
