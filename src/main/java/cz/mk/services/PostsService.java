205e9501b304e7204d24f32e63d1209b8441f1a1
karel <karel.fila@educanet.cz>
2022-11-25 11:48:32 +0100

    ukládání

--- src/main/java/cz/mk/services/PostsService.java
+++ src/main/java/cz/mk/services/PostsService.java
@@ -1,8 +1,10 @@
 package cz.mk.services;
 
+import cz.mk.interfaces.resources.IPostsResource;
 import cz.mk.interfaces.services.IPostsService;
 import cz.mk.models.Post;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +13,20 @@ import java.util.Optional;
 @ApplicationScoped
 public class PostsService implements IPostsService {
 
-    private List<Post> posts = new ArrayList<>();
+    @Inject
+    private IPostsResource postsResource;
+
+    private List<Post> posts;
 
     @Override
     public void addPost(Post post) {
         this.posts.add(post);
+        postsResource.save(posts);
     }
 
     @Override
     public void editPost(Post post) {
-
+        postsResource.save(posts);
     }
 
     @Override
@@ -28,15 +34,24 @@ public class PostsService implements IPostsService {
         Optional<Post> post = this.getPostById(id);
         if(post.isEmpty()) return;
         this.posts.remove(post.get());
+        postsResource.save(posts);
+    }
+
+    public void loadPost() {
+        if(posts == null)
+           posts = postsResource.load();
+
     }
 
     @Override
     public List<Post> getPosts() {
+        loadPost();
         return posts;
     }
 
     @Override
     public Optional<Post> getPostById(String id) {
+        loadPost();
         return this.posts.stream().filter(i -> i.getId().equals(id)).findAny();
     }
 }
