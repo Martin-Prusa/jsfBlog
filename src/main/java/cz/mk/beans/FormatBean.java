9b5f8de2ad893666db4ed554c1d8341a5443b632
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-11-25 11:16:04 +0100

    date format

--- src/main/java/cz/mk/beans/FormatBean.java
+++ src/main/java/cz/mk/beans/FormatBean.java
@@ -6,13 +6,21 @@ import org.commonmark.parser.Parser;
 import org.commonmark.renderer.Renderer;
 import org.commonmark.renderer.html.HtmlRenderer;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @ApplicationScoped
 @Named
 public class FormatBean {
     private Parser parser = Parser.builder().build();
     private HtmlRenderer renderer = HtmlRenderer.builder().build();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
 
     public String mtToHtml(String md) {
         return renderer.render(parser.parse(md));
+    }
+
+    public String formatDate(LocalDateTime date) {
+        return date.format(formatter);
     }
 }
